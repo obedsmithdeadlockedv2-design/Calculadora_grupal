88427d58dab89cb5df146afdc993de51ad8dd614
Obed <obedsmithdeadlockedv2>
2026-03-31 12:21:36 -0500

    parte_multiplicacion

--- YslaJosue_MULTIPLICACION.cs
+++ YslaJosue_MULTIPLICACION.cs
@@ -1,1 +1,26 @@
-/* Multiplicación */
+namespace CalculadoraGrupal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Multiplicación */
+
+            /* Declaración de variables */
+            int n1 = 0;
+            int n2 = 0;
+            int mul = 0;
+
+            /* Entrada de datos */
+            Console.Write("Ingrese dos números para multiplicar: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            /* Proceso */
+            mul = n1 * n2;
+
+            /* Salida */
+            Console.WriteLine("El producto de los números es " + mul); 
+        }
+    }
+}
