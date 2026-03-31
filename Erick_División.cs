c5bf0df8835233caa05d5101e1b33476f97f7337
Erick7450 <n00517495>
2026-03-31 13:08:32 -0500

    Division

---
+++ Erick_División.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Variables
+        double n1=0, n2=0, n3=0;
+        //Bienvenida
+        Console.WriteLine("Bienvenido a división ingrese los valores solicitados: ");
+        //Ingreso de Dividendo
+        Console.Write("Ingrese el dividendo(el número que desea dividir): ");
+        n1 = double.Parse(Console.ReadLine());
+        //Ingreso de Divisor
+        Console.Write("Ingrese el divisor(el número por el que desea dividir al anterior): ");
+        n2 = double.Parse(Console.ReadLine());
+        //Salida
+        n3 = n1 / n2;
+        Console.Write("El cocientede de ambos números es(resultado): ");
+        Console.Write(n3);
+    }
+}
