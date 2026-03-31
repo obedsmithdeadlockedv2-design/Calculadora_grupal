ed68ffc09b9b697d7f7c3bb970ad99cb7d3663dc
Nils21755 <nil202332@gmail.com>
2026-03-31 11:59:20 -0500

    Función suma implementada

---
+++ Suma/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+{
+    Console.Write("Ingrese el primer número: ");
+    int a = int.Parse(Console.Readline());
+    Console.Write ("Ingrese el segundo número: ");
+    int b = int.Parse(Console.Readline());
+    int suma = a+b;
+    Console.WriteLine("La suna es: " + suma);
+}
+}
