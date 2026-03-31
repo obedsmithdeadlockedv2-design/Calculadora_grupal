d7b60d302c5b7e2a8f1fadcad7b4b7996856a4f4
Nils-hash1 <N00509073@upn.pe>
2026-03-31 13:47:01 -0500

    Suma

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+{
+    Console.Write("Ingrese el primer número: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write ("Ingrese el segundo número: ");
+    int b = int.Parse(Console.ReadLine());
+    int suma = a+b;
+    Console.WriteLine("La suna es: " + suma);
+}
+}
